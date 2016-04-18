32848976d7e57c0adc956f5002a8b024ac8c7934
Alvaro Cantador <alvaro.inckot@pinho.com.br>
2016-04-18 15:26:59 -0300

    add dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :controllers
 
   scope "(:locale)", locale: /pt-BR|en/ do
     root 'welcome#index'
+    get '/dashboard' => 'dashboard#index' 
   end
 
 end
