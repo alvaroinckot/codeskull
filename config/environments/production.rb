d0e5a3df7730084ebfdf6c87cab4ffdfee651821
Alvaro Inckot <alvaro.inckot@pinho.com.br>
2016-07-16 20:17:42 -0300

    Add files to asset precompile filter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+  
 end
