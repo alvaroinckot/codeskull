1ad769791fc86b9d2b3e04316f4b0bee141bae48
Alvaro Cantador <alvaroinckot@gmail.com>
2016-10-10 11:39:18 -0300

    added to_code method to expectations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,13 +2,17 @@ class Activity < ActiveRecord::Base
 
   belongs_to :task
   belongs_to :grade
-  
+
   attr_accessor :console
 
   def compile(params)
 
     response = environment.compile(pre_process_source) # todo check ouput expecs
-    self.console = response
+    self.console = response["output"]
+      .select { |output| output.include? "{" }
+      .map do |c| 
+        JSON.parse(c) 
+      end
 
     # self.console = "Compilado com sucesso!\n"
 	 # self.completed = true # validate expectations
@@ -21,7 +25,7 @@ class Activity < ActiveRecord::Base
     private
 
       def pre_process_source
-        self.source_code # => bind with expectations.to_code
+        self.source_code + self.task.expectations.map { |exp| exp.to_code }.inject { |pre, post| pre + post }
       end
 
       def check_var_expectations
--- app/models/expectation.rb
+++ app/models/expectation.rb
@@ -1,3 +1,13 @@
 class Expectation < ActiveRecord::Base
   belongs_to :task
+
+  # TODO add support to many languages
+  def to_code
+  	"\r\n puts  \"{\" + " +
+  	"\"\\\"\" + (#{self.id}).to_s + \"\\\"\"  " + 
+  	"+ \":\" +  " +
+  	"\"\\\"\" + (#{self.name} #{self.operator} #{self.value}).to_s + " +
+  	"\"\\\"\"+ \"}\"  "
+  end
+
 end
